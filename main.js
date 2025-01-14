477cde99a40f53d5bbd09c8b08477159710e56c6
yjc0901 <yayaz96@naver.com>
2025-01-14 15:53:02 +0900

    로그인 수정

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         sameSite: 'strict',
         maxAge: 1000 * 60 * 60 // 1시간
